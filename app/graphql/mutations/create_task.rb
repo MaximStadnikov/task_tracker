0145c3d93b9c840146263a0b56d6da24dbcbcafc
DataJoshua <rodriguez.taco.joshua@gmail.com>
2022-11-28 23:12:29 +0300

    update_comments mutation

--- app/graphql/mutations/create_task.rb
+++ app/graphql/mutations/create_task.rb
@@ -5,7 +5,7 @@ module Mutations
     type Types::Payloads::CreateTaskPayload
 
     def resolve(input:)
-      project = Project.find(input[:project_id])
+      project = Project.find(input.project_id)
       result = Tasks::Create.call(project: project, task_params: input.to_h)
 
       if result.success?
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
     field :create_task, mutation: Mutations::CreateTask
+    field :update_comment, mutation: Mutations::UpdateComment
   end
 end
