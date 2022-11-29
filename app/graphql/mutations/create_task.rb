4ba840b2bc462eda1f6db5acf7612abda27a957c
DataJoshua <rodriguez.taco.joshua@gmail.com>
2022-11-29 22:43:52 +0300

    create task mutation

--- app/graphql/mutations/create_task.rb
+++ app/graphql/mutations/create_task.rb
@@ -1,17 +1,19 @@
 module Mutations
   class CreateTask < BaseMutation
+    include GraphqlErrors
+
     argument :input, Types::Inputs::CreateTaskInput, required: true
 
     type Types::Payloads::CreateTaskPayload
 
     def resolve(input:)
       project = Project.find(input.project_id)
-      result = Tasks::Create.call(project: project, task_params: input.to_h)
+      result = Tasks::Create.call(project: project, task_params: input.to_h.except(:project_id))
 
       if result.success?
         result.to_h.merge({ errors: [] })
       else
-        result.to_h.merge({ errors: prepared_params(result.project) })
+        result.to_h.merge({ errors: formatted_errors(result.project) })
       end
     end
   end
