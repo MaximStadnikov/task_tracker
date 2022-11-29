5effe83d8bbc928ee37e93544b0b89d8fcb3de48
DataJoshua <rodriguez.taco.joshua@gmail.com>
2022-11-29 22:29:48 +0300

    create Task mutation

--- app/graphql/types/inputs/update_comment_input.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  module Inputs
-    class UpdateCommentInput < Types::BaseInputObject
-      argument :id, ID, required: true
-      argument :content, String, required: true
-    end
-  end
-end
