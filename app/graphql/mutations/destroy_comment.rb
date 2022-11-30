40e1a88c269a05bcab124063262545c19b9053d6
DataJoshua <rodriguez.taco.joshua@gmail.com>
2022-11-30 16:22:23 +0300

    rubocop

--- app/graphql/mutations/destroy_comment.rb
+++ app/graphql/mutations/destroy_comment.rb
@@ -3,18 +3,17 @@ module Mutations
     include GraphqlErrors
 
     argument :input, Types::Inputs::DestroyCommentInput, required: true
-    
+
     type Types::Payloads::DestroyCommentPayload
 
     def resolve(input:)
-        comment = Comment.find(input.comment_id)
-        result = Comments::Destroy.call(comment: comment)
-        if result.success?
-            result.to_h
-        else
-            result.to_h.merge({errros: formatted_errors(result.comment)})
-        end
+      comment = Comment.find(input.comment_id)
+      result = Comments::Destroy.call(comment: comment)
+      if result.success?
+        result.to_h
+      else
+        result.to_h.merge({ errros: formatted_errors(result.comment) })
+      end
     end
-
   end
 end
