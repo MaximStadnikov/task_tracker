daf64f7cdff581880edc5f3e1ca4cc0f03c7d42d
= <=>
2022-12-04 14:40:05 +0300

    added destroyProject

---
+++ app/graphql/types/inputs/destroy_project_input.rb
@@ -0,0 +1,9 @@
+module Types
+    module Inputs
+      class DestroyProjectInput < Types::BaseInputObject
+        argument :id, ID, required: true
+       
+      end
+    end
+  end
+  
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
     field :update_project, mutation: Mutations::UpdateProject
+    field :destroy_project, mutation: Mutations::DestroyProject
+    field :create_comment, mutation: Mutations::CreateComment
   end
 end
