c687f7114ff47f698c4c2b8ce83b57ebdd85a039
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2022-12-04 11:57:39 +0300

    Merge remote-tracking branch 'origin/master' into lesson13

--- app/graphql/types/inputs/update_project_input.rb
+++ app/graphql/types/inputs/update_project_input.rb
@@ -2,7 +2,7 @@ module Types
   module Inputs
     class UpdateProjectInput < Types::BaseInputObject
       argument :id, ID, required: true
-      argument :name, String, required: true
+      argument :name, String, required: false
       argument :description, String, required: false
     end
   end
