200e403a09d938b184c84c6d0e2fe21521e1a898
DataJoshua <rodriguez.taco.joshua@gmail.com>
2022-12-12 14:34:18 +0300

    Payload corrected

--- app/graphql/types/payloads/update_comment_payload.rb
+++ app/graphql/types/payloads/update_comment_payload.rb
@@ -2,6 +2,7 @@ module Types
   module Payloads
     class UpdateCommentPayload < Types::BaseObject
       field :comment, Types::CommentType, null: false
+      field :errors, [Types::UserError], null: true
     end
   end
 end
