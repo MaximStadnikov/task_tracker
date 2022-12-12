1698427e604209618d3d6fb816640f09105a25d6
DataJoshua <rodriguez.taco.joshua@gmail.com>
2022-12-12 14:30:36 +0300

    resolve create_task mutation

--- app/graphql/mutations/create_task.rb
+++ app/graphql/mutations/create_task.rb
@@ -9,12 +9,7 @@ module Mutations
     def resolve(input:)
       project = Project.find(input.project_id)
       result = Tasks::Create.call(project: project, task_params: input.to_h.except(:project_id))
-
-      if result.success?
-        result.to_h.merge({ errors: [] })
-      else
-        result.to_h.merge({ errors: formatted_errors(result.project) })
-      end
+      result.to_h.merge(errors: formatted_errors(result.task))
     end
   end
 end
